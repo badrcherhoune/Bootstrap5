d800e72cebc2cbdc4f97fbc18ab4e0958f2293b6
Your Name <cherhoune@badr.com>
2023-07-04 14:51:15 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './componant/register/register.component';
+import { ListComponent } from './componant/list/list.component';
+import { LoginComponent } from './componant/login/login.component';
+import { NavbarComponent } from './componant/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component:RegisterComponent},
+  {path: 'navbar', component:NavbarComponent , children: [
+    {path: 'list', component:ListComponent}
+  ]},
+  
+  {path: '', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componant/navbar/navbar.component';
+import { ListComponent } from './componant/list/list.component';
+import { RegisterComponent } from './componant/register/register.component';
+import { LoginComponent } from './componant/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
